3c2ce0b4184e2c16b123dd5afe3af6a24311f4c3
Nate B <nathanbrengman@gmail.com>
2022-06-30 21:26:24 -0500

    added questions

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var questions = [
+    { question: "Question #1: Which of the following is not a value type?", answers: ["String", "Boolean", "Alert", "Number"], correct: "Alert" },
+  
+    { question: "Question #2: What are variables in an array called?", answers: ["Objects", "Strings", "Arguments", "None of the above"], correct: "Arguments" },
+  
+    { question: "Question #3: setInterval() method calls a function at specified intervals of how long?", answers: ["Seconds", "Hundredths", "Thousandths", "milliseconds"], correct: "milliseconds" },
+  
+    { question: "Question #4: Which of these symbols checks type and value?", answers: ["%", "==", "||", "==="], correct: "===" },
+  
+    { question: "Question #5: What does ! mean?", answers: ["Not", "Or", "Undefined", "Null"], correct: "Not" },
+  ];
+  
+ 
